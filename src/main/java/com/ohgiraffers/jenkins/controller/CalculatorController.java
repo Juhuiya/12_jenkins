4856f8120d6b6584d8eb59498642240d646f6fa9
juhui <juya202106@naver.com>
2025-07-03 10:39:27 +0900

    style: 컨트롤러 소스 코드 이모니콘 변경

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!😁🐱";
+        return "I'm alive!😁🐋";
     }
 
     @GetMapping("/plus")
