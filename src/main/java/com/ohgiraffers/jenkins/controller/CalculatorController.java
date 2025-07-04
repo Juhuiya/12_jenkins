ecb5028c93341397bca58106274383c3289147c9
juhui <juya202106@naver.com>
2025-07-04 10:12:47 +0900

    style: 변화확인 하기 위한 컨트롤러 소스 코드 변경

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!😁🐋";
+        return "ArgoCD Change!😁🐋";
     }
 
     @GetMapping("/plus")
